875d915573b157461f92e67349b1c25899a48e51
ielbadisy <elbadisyimad@gmail.com>
2025-05-27 11:49:48 +0100

    remove old versions

--- R/safe.R
+++
@@ -1,7 +0,0 @@
-safe <- function(.f) {
-  function(...) {
-    result <- tryCatch(list(result = .f(...), error = NULL),
-                       error = function(e) list(result = NULL, error = e))
-    return(result)
-  }
-}
