9a0e2b7288c22dbb8b59e3652f3d7b004fa6d98f
ielbadisy <elbadisyimad@gmail.com>
2025-05-27 11:48:07 +0100

    add fmapn()

--- R/fmapn.R
+++ R/fmapn.R
@@ -1,4 +1,6 @@
-## pmap
-fmapn<- function(.l, .f, ...) {
-  do.call(Map, c(list(.f), .l, ...))
+
+fmapn <- function(.l, .f, ncores = NULL, pb = FALSE, ...) {
+  .f <- match.fun(.f)
+  args <- do.call(Map, c(f = list(function(...) list(...)), .l))
+  fapply(args, function(x) do.call(.f, x), ncores = ncores, pb = pb, ...)
 }
