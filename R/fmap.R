393938d2ad792347a3c070a22ed3754235669c18
ielbadisy <elbadisyimad@gmail.com>
2025-05-30 12:16:12 +0100

    bench fmap() vs furrr::future_map()

--- R/fmap.R
+++ R/fmap.R
@@ -4,3 +4,29 @@ fmap <- function(.x, .f, ncores = NULL, pb = FALSE, ...) {
   fapply(.x, .f, ncores = ncores, pb = pb, ...)
 }
 
+
+
+library(furrr)
+library(bench)
+library(future)
+
+slow_fn <- function(x) {
+  Sys.sleep(0.01)
+  x^2
+}
+
+x <- 1:100
+
+ncores <- 12
+
+plan(multisession, workers = ncores)
+
+results <- bench::mark(
+  funr_fmap      = fmap(x, slow_fn, ncores = ncores, pb = TRUE),
+  furrr_future   = furrr::future_map(x, slow_fn, .options = furrr::furrr_options(seed = NULL)),
+  iterations = 3,
+  check = FALSE,
+  memory = FALSE
+)
+
+print(results)
