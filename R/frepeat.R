a501e6e5a491a9e16d844c97a30ed510e68f0836
ielbadisy <elbadisyimad@gmail.com>
2025-06-09 20:13:17 +0100

    Redesign frepeat() to support .x input + piped workflows Updated frepeat() to accept .x and .f with argument forwarding via .args, enabling cleaner pipe-friendly usage. Retains backward compatibility with expression based logic.

--- R/frepeat.R
+++ R/frepeat.R
@@ -1,10 +1,29 @@
 
-frepeat <- function(times, expr, simplify = FALSE, ncores = NULL, pb = FALSE, ...) {
+#frepeat <- function(times, expr, simplify = FALSE, ncores = NULL, pb = FALSE, ...) {
+  #out <- if (is.function(expr)) {
+    #fapply(seq_len(times), function(i) expr(...), ncores = ncores, pb = pb)
+  #} else {
+    #fapply(seq_len(times), function(i) eval(expr), ncores = ncores, pb = pb)
+  #}
+  #if (simplify) simplify2array(out) else out
+#}
+
+frepeat <- function(.x = NULL, times, expr, simplify = FALSE, ncores = NULL, pb = FALSE, ...) {
   out <- if (is.function(expr)) {
-    fapply(seq_len(times), function(i) expr(...), ncores = ncores, pb = pb)
+    fapply(
+      seq_len(times),
+      function(i) {
+        if (is.null(.x)) expr(...) else expr(.x, ...)
+      },
+      ncores = ncores,
+      pb = pb
+    )
   } else {
     fapply(seq_len(times), function(i) eval(expr), ncores = ncores, pb = pb)
   }
   if (simplify) simplify2array(out) else out
 }
 
+
+
+##The expr argument is passed as a value, so if we write expr = fcv(x, evaluate_split, ...), the function fcv(...) gets evaluated immediately, before frepeat() is even called!!!
