3590e8ca75765aea0c961c2912d4d5aab19e7f3b
ielbadisy <elbadisyimad@gmail.com>
2025-06-13 00:48:18 +0100

    centralize helper functions in helpers.R

--- R/fapply.R
+++ R/fapply.R
@@ -11,47 +11,8 @@ fapply <- function(.x, .f, ncores = 1, pb = FALSE, ...) {
   if (!is.vector(.x) || is.object(.x)) .x <- as.list(.x)
   if (!length(.x)) return(list())
 
-  # custom progress bar function
-  funr_progress_bar <- function(min = 0, max = 1, style = 1, width = NA, char = "=") {
-    start_time <- proc.time()[["elapsed"]]
-    i <- min
-    if (is.na(width)) width <- getOption("width")
-    get_time_str <- function(seconds) {
-      sec <- round(seconds %% 60)
-      minutes <- floor(seconds / 60) %% 60
-      hours <- floor(seconds / 3600)
-      sprintf("%02ih %02im %02is", hours, minutes, sec)
-    }
-    update <- function(value) {
-      if (!is.finite(value) || value < min || value > max) return()
-      i <<- value
-      elapsed <- proc.time()[["elapsed"]] - start_time
-      total <- max - min
-      percent <- (i - min) / total
-      remaining <- if (percent > 0) elapsed * (1 - percent) / percent else NA
-      bar_width <- width - 30
-      done <- floor(bar_width * percent)
-      left <- bar_width - done
-      cat(sprintf("\r |%s%s| %3d%% elapsed=%s, remaining~%s",
-                  strrep(char, done), strrep(" ", left),
-                  round(percent * 100),
-                  get_time_str(elapsed),
-                  if (!is.na(remaining)) get_time_str(remaining) else "..."))
-      flush.console()
-    }
-    kill <- function() cat("\n")
-    update(i)
-    list(up = update, kill = kill)
-  }
 
   if (pb) {
-    splitpb <- function(nx, ncl, nout = 100) {
-      i <- seq_len(nx)
-      if (ncl == 0L) return(list())
-      k <- max(1L, ceiling(ceiling(nx / ncl) / nout))
-      g <- 1L + (i - 1L) %/% as.integer(ncl * k)
-      structure(split(i, g), names = NULL)
-    }
     Split <- splitpb(length(.x), ncores, nout = 100)
     B <- length(Split)
     pb_bar <- funr_progress_bar(min = 0, max = B)
