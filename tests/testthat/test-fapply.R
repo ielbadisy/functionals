14dfc4b035cbe816660abb1768bd25ec4fabd0ac
ielbadisy <elbadisyimad@gmail.com>
2025-06-13 11:12:57 +0100

    add unit tests for fapply()

--- tests/testthat/test-fapply.R
+++ tests/testthat/test-fapply.R
@@ -1,54 +1,15 @@
-test_that("fapply works sequentially without progress bar", {
+test_that("fapply works sequentially", {
+  x <- 1:5
+  expect_equal(fapply(x, function(x) x^2), as.list(x^2))
+})
+
+test_that("fapply works with progress bar and parallel", {
+  skip_on_cran()
   x <- 1:10
-  expect_equal(fapply(x, function(i) i^2), as.list(x^2))
+  res <- fapply(x, function(x) x + 1, ncores = 2, pb = TRUE)
+  expect_equal(res, as.list(x + 1))
 })
 
-test_that("fapply works sequentially with progress bar", {
-  x <- 1:10
-  expect_equal(fapply(x, function(i) i^2, pb = TRUE), as.list(x^2))
-})
-
-test_that("fapply works in parallel without progress bar", {
-  skip_if(.Platform$OS.type == "windows", "mclapply not supported on Windows")
-  x <- 1:10
-  expect_equal(fapply(x, function(i) i^2, ncores = 2), as.list(x^2))
-})
-
-test_that("fapply works in parallel with progress bar", {
-  skip_if(.Platform$OS.type == "windows", "mclapply not supported on Windows")
-  x <- 1:10
-  expect_equal(fapply(x, function(i) i^2, ncores = 2, pb = TRUE), as.list(x^2))
-})
-
-test_that("fapply returns empty list when input is empty", {
-  expect_equal(fapply(numeric(0), identity), list())
-})
-
-test_that("fapply warns and defaults on invalid ncores", {
-  expect_warning(
-    res <- fapply(1:5, identity, ncores = "bad"),
-    regexp = "ncores.*positive integer"
-  )
-  expect_equal(res, as.list(1:5))
-})
-
-test_that("fapply warns and disables progress on invalid pb", {
-  expect_warning(
-    res <- fapply(1:5, identity, pb = "yes"),
-    regexp = "pb.*TRUE or FALSE"
-  )
-  expect_equal(res, as.list(1:5))
-})
-
-test_that("fapply handles character input and side effects", {
-  expect_equal(
-    fapply(letters[1:3], toupper),
-    as.list(c("A", "B", "C"))
-  )
-})
-
-test_that("fapply works with additional arguments", {
-  x <- 1:3
-  square_add <- function(i, add) (i^2) + add
-  expect_equal(fapply(x, square_add, add = 1), as.list(x^2 + 1))
+test_that("fapply handles empty input", {
+  expect_equal(fapply(list(), identity), list())
 })
