cf56b5b9aa823ce634af1e8db974dd43af33eeb5
ielbadisy <elbadisyimad@gmail.com>
2025-06-13 11:27:41 +0100

    fix: stroe fapply() result before returning invisibly

--- R/fwalk.R
+++ R/fwalk.R
@@ -27,6 +27,7 @@
 
 fwalk <- function(.x, .f, ncores = NULL, pb = FALSE, ...) {
   .f <- match.fun(.f)
-  fapply(.x, function(x) {.f(x, ...); NULL}, ncores = ncores, pb = pb)
+  tmp <- fapply(.x, function(x) {.f(x, ...); NULL}, ncores = ncores, pb = pb)
   invisible(.x)
 }
+
