4c223af8f0ad9a7ecae7871e1b77f84439588ce5
ielbadisy <elbadisyimad@gmail.com>
2025-06-13 11:20:26 +0100

    add unit tests for fcv()

--- tests/testthat/test-fcv.R
+++ tests/testthat/test-fcv.R
@@ -1,3 +1,26 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("fcv works with rsample::vfold_cv splits", {
+  library(rsample)
+  set.seed(123)
+  cv <- vfold_cv(mtcars, v = 5)
+
+  results <- fcv(cv$splits, function(split) {
+    dat <- analysis(split)
+    mean(dat$mpg)
+  })
+
+  expect_length(results, 5)
+  expect_true(all(sapply(results, is.numeric)))
+})
+
+test_that("fcv supports parallel execution with progress", {
+  skip_on_cran()
+  library(rsample)
+  cv <- vfold_cv(mtcars, v = 4)
+
+  results <- fcv(cv$splits, function(split) {
+    nrow(analysis(split))
+  }, ncores = 2, pb = TRUE)
+
+  expect_length(results, 4)
+  expect_true(all(sapply(results, is.numeric)))
 })
