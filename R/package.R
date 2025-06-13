3502ff1907f6f33b2f69fe4f9d73b83f7f6d9aa5
ielbadisy <elbadisyimad@gmail.com>
2025-06-13 09:54:15 +0100

    doc: add package level documentation file with @docType and @keywords tags

---
+++ R/package.R
@@ -0,0 +1,9 @@
+#' funr: Functional Programming with Parallelism in R
+#'
+#' This package provides a consistent and intuitive API for applying functions
+#' over lists, vectors, and data frames, with built-in support for parallelism
+#' and progress bars.
+#'
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
