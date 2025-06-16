2b125d53f0ebcb690c3e29c3e194f69814166409
ielbadisy <elbadisyimad@gmail.com>
2025-06-16 23:44:10 +0100

    add freduce()

--- R/freduce.R
+++ R/freduce.R
@@ -1,27 +1,30 @@
-#' Reduce a list using a binary function
+#' Functional reduce
 #'
-#' Applies a binary function `.f` cumulatively to the elements of `.x`, reducing it to a single value.
+#' Apply a binary function iteratively over a list or vector, reducing it to a single value.
+#' This is a functional programming wrapper around [Reduce()] with support for left-to-right
+#' and right-to-left reduction and an optional initial value.
 #'
-#' @param .x A list or atomic vector.
-#' @param .f A binary function to reduce over elements of `.x`.
-#' @param .init Optional initial value. Passed to `Reduce()` as `init`.
-#' @param .right Logical. If `TRUE`, reduction proceeds right-to-left. Default is `FALSE`.
-#' @param ... Additional arguments passed to `.f`.
+#' @param .x A vector or list of elements to reduce.
+#' @param .f A binary function to apply. Can be specified as a function or a function name (e.g., `\`+\``).
+#' @param .init An optional initial value to start the reduction. If `NULL`, reduction begins with the first two elements of `.x`.
+#' @param .right Logical. If `FALSE` (default), reduction proceeds left-to-right. If `TRUE`, right-to-left reduction is used.
 #'
-#' @return A single value obtained by cumulatively applying `.f`.
+#' @return The result of reducing `.x` using `.f`, optionally starting from `.init`.
 #'
 #' @examples
-#' freduce(1:5, `+`)           # 1 + 2 + 3 + 4 + 5 = 15
-#' freduce(1:5, paste0)        # "12345"
-#' freduce(list(1, 2, 3), function(x, y) x + y)  # 6
+#' freduce(1:5, `+`)                          # Sum: 1 + 2 + 3 + 4 + 5 = 15
+#' freduce(letters[1:4], paste0)             # Concatenate: "abcd"
+#' freduce(list(1, 2, 3), `*`)               # Product: 1 * 2 * 3 = 6
+#' freduce(1:3, `+`, .init = 10)             # Sum with initial value: 10 + 1 + 2 + 3 = 16
+#' freduce(1:3, paste0, .right = TRUE)       # Right-to-left: paste0(1, paste0(2, 3)) = "123"
 #'
 #' @export
-
-freduce <- function(.x, .f, .init, .right = FALSE, ...) {
+freduce <- function(.x, .f, .init = NULL, .right = FALSE) {
   .f <- match.fun(.f)
-  if (missing(.init)) {
-    Reduce(.f, .x, right = .right, ...)
+  if (!is.list(.x)) .x <- as.list(.x)
+  if (is.null(.init)) {
+    Reduce(.f, .x, right = .right)
   } else {
-    Reduce(.f, .x, init = .init, right = .right, ...)
+    Reduce(.f, .x, init = .init, right = .right)
   }
 }
