d70352e441439bc5078eb1b403ac105516d67e30
ielbadisy <elbadisyimad@gmail.com>
2025-06-18 10:09:41 +0100

    add unit test for floop()

---
+++ tests/testthat/test-floop.R
@@ -0,0 +1,15 @@
+test_that("floop returns expected results", {
+  out <- floop(1:3, function(x) x + 1)
+  expect_equal(out, list(2, 3, 4))
+})
+
+test_that("floop respects .capture = FALSE", {
+  calls <- character()
+  invisible(floop(1:3, function(i) calls <<- c(calls, paste0("log", i)), .capture = FALSE))
+  expect_equal(calls, c("log1", "log2", "log3"))
+})
+
+test_that("floop works with ncores > 1", {
+  out <- floop(1:4, function(x) x^2, ncores = 2)
+  expect_equal(out, list(1, 4, 9, 16))
+})
