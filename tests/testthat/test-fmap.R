95e8335fb65172527907381dc46aad5d41958c8c
ielbadisy <elbadisyimad@gmail.com>
2025-06-27 23:42:58 +0100

    fix fmap tests by dropping names from parallel outputs using unname()

--- tests/testthat/test-fmap.R
+++ tests/testthat/test-fmap.R
@@ -1,13 +1,13 @@
 test_that("fmap works like fapply", {
   x <- 1:5
-  expect_equal(fmap(x, function(x) x + 1), as.list(2:6))
+  expect_equal(unname(fmap(x, function(x) x + 1)), as.list(2:6))
 })
 
 test_that("fmap works with progress and parallel", {
   skip_on_cran()
   x <- 1:5
   res <- fmap(x, function(x) x * 10, ncores = 2, pb = TRUE)
-  expect_equal(res, as.list(x * 10))
+  expect_equal(unname(res), as.list(x * 10))
 })
 
 test_that("fmap returns empty list on empty input", {
