a62934ab7824acaf406379eac42fee815c7eb270
ielbadisy <elbadisyimad@gmail.com>
2025-06-27 23:42:31 +0100

    wrap fapply outputs in unname() to match expected list structure

--- tests/testthat/test-fapply.R
+++ tests/testthat/test-fapply.R
@@ -1,13 +1,13 @@
 test_that("fapply works sequentially", {
   x <- 1:5
-  expect_equal(fapply(x, function(x) x^2), as.list(x^2))
+  expect_equal(unname(fapply(x, function(x) x^2)), as.list(x^2))
 })
 
 test_that("fapply works with progress bar and parallel", {
   skip_on_cran()
   x <- 1:10
   res <- fapply(x, function(x) x + 1, ncores = 2, pb = TRUE)
-  expect_equal(res, as.list(x + 1))
+  expect_equal(unname(res), as.list(x + 1))
 })
 
 test_that("fapply handles empty input", {
